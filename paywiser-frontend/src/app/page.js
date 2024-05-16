06f58b11129e628ffce1ec65e76d5b4bee080aa7
Miles <mithomtab@gmail.com>
2024-05-16 10:48:54 -0400

    Update
    new

--- paywiser-frontend/src/app/page.js
+++ paywiser-frontend/src/app/page.js
@@ -1,28 +1,22 @@
 // src/app/page.js
 import "./home.css";
 
-
-
 export default function Home() {
   return (
     <main>
-    
-        {/* Other movie elements */}
-        <div class="rounded-box">
-        <img src = "PayWise-home-logo.png" alt="PayWiser Home Logo" className="box-image"/>
-        <div className="title">PayWiser</div> 
-       PayWiser is an app that allows debt management to be 
-       worry free. Here, you can track multiple debts,
-       receive personalized repayment stratiegies and plans, and stay on to of
-       your financial goals. Join PayWiser to take control of your debt and
-       achieve financial confidence
-       <div className="TitleC">
-      </div>
+      <div className="rounded-box">
+        <img
+          src="./PayWise-home-logo.png"
+          alt="PayWiser Home Logo"
+          className="box-image"
+        />
+        <div className="title">PayWiser</div>
+        PayWiser is an app that allows debt management to be worry-free. Here,
+        you can track multiple debts, receive personalized repayment strategies
+        and plans, and stay on top of your financial goals. Join PayWiser to
+        take control of your debt and achieve financial confidence
+        <div className="TitleC"></div>
       </div>
     </main>
   );
-
 }
-
-  
-
