503c302bd80d2a9bf1d2dbf4bd515aebc56f7a8e
Katherine Boiatey <kob1197@rit.edu>
2024-05-16 01:00:37 -0400

    page 1

--- paywiser-frontend/src/app/page.js
+++ paywiser-frontend/src/app/page.js
@@ -9,13 +9,13 @@ export default function Home() {
     
         {/* Other movie elements */}
         <div class="rounded-box">
-           
+        <img src = "PayWise-home-logo.png" alt="PayWiser Home Logo" className="box-image"/>
         <div className="title">PayWiser</div> 
        PayWiser is an app that allows debt management to be 
        worry free. Here, you can track multiple debts,
        receive personalized repayment stratiegies and plans, and stay on to of
        your financial goals. Join PayWiser to take control of your debt and
-       achieve financial confidence!
+       achieve financial confidence
        <div className="TitleC">
       </div>
       </div>
