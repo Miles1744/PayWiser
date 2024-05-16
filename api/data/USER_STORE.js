d7780090551365cb5b73919d369a37d09d5164ee
Miles <mithomtab@gmail.com>
2024-05-15 22:50:46 -0400

    Update
    new

---
+++ api/data/USER_STORE.js
@@ -0,0 +1,40 @@
+const MOVIES = [
+  {
+    id: 1,
+    username: "Miles",
+    Debt: 3500,
+    Cards: {
+      Debit_Card_1: ["Car Payments", 4500, 300, 1.2, 54.3, 12],
+
+      Debit_Card_2: ["Car Payments", 4500, 300, 1.2, 54.3, 12],
+
+      Debit_Card_3: ["Car Payments", 4500, 300, 1.2, 54.3, 12],
+    },
+  },
+  {
+    id: 2,
+    name: "Phil",
+    Debt: 1000,
+    Cards: {
+      Debit_Card_1: ["Car Payments", 4500, 300, 1.2, 54.3, 12],
+
+      Debit_Card_2: ["Car Payments", 4500, 300, 1.2, 54.3, 12],
+
+      Debit_Card_3: ["Car Payments", 4500, 300, 1.2, 54.3, 12],
+    },
+  },
+  {
+    id: 3,
+    name: "Mark",
+    Debt: 2000,
+    Cards: {
+      Debit_Card_1: ["Car Payments", 4500, 300, 1.2, 54.3, 12],
+
+      Debit_Card_2: ["Car Payments", 4500, 300, 1.2, 54.3, 12],
+
+      Debit_Card_3: ["Car Payments", 4500, 300, 1.2, 54.3, 12],
+    },
+  },
+];
+
+module.exports = MOVIES;
