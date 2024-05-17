1f18647220946b2a843411d0112c38344f792cb9
Joshua O. <156972833+Jcokuad@users.noreply.github.com>
2024-05-17 08:44:20 -0400

    Updated Login Page

--- paywiser-frontend/src/app/Login/page.js
+++ paywiser-frontend/src/app/Login/page.js
@@ -1,7 +1,25 @@
 'use client';
 
+import "./Login.css";
+
+
+
 const Login = () => {
-    return <div>Login</div>;
-  };
   
-  export default Login;
+  return (
+    <div className="Login-container">
+      <h2>Welcome to PayWiser!</h2>
+      <p>Let's help you get registered!</p>
+      <form className="Login-form">
+        <input type="text" placeholder="Enter username" className="input-field" />
+        <input type="email" placeholder="Enter email" className="input-field" />
+        <input type="password" placeholder="Enter password" className="input-field" />
+        <input type="password" placeholder="Confirm password" className="input-field" />
+        <button type="submit" className="Login-button">Register</button>
+      </form>
+      <p className="signin-link">Already have an account? <a href="/Login">Sign in</a></p>
+    </div>
+  );
+};
+
+export default Login;
